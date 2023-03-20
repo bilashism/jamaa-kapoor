acc7c2d11c95efdea40db4e7b67d2e0fdda9764a
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2023-03-20 22:26:21 +0600

    router setup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,19 +2,9 @@ import React, { useState } from 'react';
 import Header from './Header';
 
 const Home = () => {
-  const [count, setCount] = useState(0);
-  if (count > 5) throw new Error('oops');
-
   return (
     <>
       <Header />
-      <button
-        onClick={() => {
-          setCount(prev => prev + 1);
-        }}
-      >
-        {count}
-      </button>
     </>
   );
 };
