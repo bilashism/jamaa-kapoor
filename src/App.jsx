da23278610697076c6d477e1fee27995a162eee6
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2023-03-21 09:56:55 +0600

    setup toaster

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Toaster } from 'react-hot-toast';
 import { RouterProvider } from 'react-router-dom';
 
 import router from './routes/router';
 
-const App = () => <RouterProvider router={router} />;
+const App = () => (
+  <>
+    <RouterProvider router={router} />
+    <Toaster />
+  </>
+);
 
 export default App;
